15d896982276b6d2c38fc1b33487bd6b177bb1db
Miles McGuire <miles@milesmcguire.com>
2014-02-09 17:03:08 +0000

    Bare bones initial memory
    Cannot serve as 'real' memory, rather is just a list of instructions to execute

---
+++ src/Memory.hs
@@ -0,0 +1,11 @@
+module Handy.Memory where
+
+{--
+    Place holder for the actual memory implementation.
+
+    TODO: Decide on memory architecture
+--}
+
+import Handy.Instructions
+import Handy.Registers
+type Memory = [Instruction]
