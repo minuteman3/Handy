3962da9ad30afe6325dda31810b424981c017444
Miles McGuire <miles@milesmcguire.com>
2014-02-09 23:15:00 +0000

    Rename CPSR to StatusRegister
    In the interest of clarity, mainly

--- src/StatusRegister.hs
+++ src/StatusRegister.hs
@@ -1,24 +1,30 @@
-module Handy.CPSR where
+module Handy.StatusRegister where
 
 import Data.Bits
 import Data.Int (Int32)
 
-data CPSR = CPSR { negative :: Bool
-                 , zero     :: Bool
-                 , carry    :: Bool
-                 , overflow :: Bool
-                 } deriving Show
+data StatusRegister = StatusRegister { negative :: Bool
+                                     , zero     :: Bool
+                                     , carry    :: Bool
+                                     , overflow :: Bool
+                                     } deriving Show
 
-toConstant :: CPSR -> Int32
+blankStatusRegister = StatusRegister { negative = False
+                                     , zero     = False
+                                     , carry    = False
+                                     , overflow = False
+                                     }
+
+toConstant :: StatusRegister -> Int32
 toConstant cpsr = n .|. z .|. c .|. v
                 where n = if negative cpsr then bitDefault 31 else 0
                       z = if zero cpsr then bitDefault 30 else 0
                       c = if carry cpsr then bitDefault 29 else 0
                       v = if overflow cpsr then bitDefault 28 else 0
 
-fromConstant :: Int32 -> CPSR
-fromConstant val = CPSR { negative = testBitDefault val 31
-                        , zero     = testBitDefault val 30
-                        , carry    = testBitDefault val 29
-                        , overflow = testBitDefault val 28
-                        }
+fromConstant :: Int32 -> StatusRegister
+fromConstant val = StatusRegister { negative = testBitDefault val 31
+                                  , zero     = testBitDefault val 30
+                                  , carry    = testBitDefault val 29
+                                  , overflow = testBitDefault val 28
+                                  }
