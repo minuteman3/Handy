ad855f9279596ec4285907820b062115678ea856
Miles McGuire <miles@milesmcguire.com>
2014-02-09 17:32:22 +0000

    Use GADTs for better type safety in instructions
    Using GADTs here lets the Haskell compiler catch erroneously constructed instructions

--- src/CPU.hs
+++ src/CPU.hs
@@ -25,35 +25,38 @@ incPC = do machine <- get
            setRegister Reg.PC $ ((registers machine) `Reg.get` Reg.PC) + 1
 
 execute :: Instruction -> Run ()
-execute (MOV dest src) = do machine <- get
-                            case src of
-                                (ArgC v) -> setRegister dest v
-                                (ArgR r) -> setRegister dest (registers machine `Reg.get` r)
-                            incPC
-                            run
-
-execute (ADD dest src1 src2) = executeBinArith (+) dest src1 src2
-execute (SUB dest src1 src2) = executeBinArith (-) dest src1 src2
-execute (RSB dest src1 src2) = executeBinArith (-) dest src2 src1
-execute (MUL dest src1 src2) = executeBinArith (*) dest src1 (ArgR src2)
-
+execute (MOV dest src)       = executeUnOp  id  dest src
+execute (ADD dest src1 src2) = executeBinOp (+) dest src1 src2
+execute (SUB dest src1 src2) = executeBinOp (-) dest src1 src2
+execute (RSB dest src1 src2) = executeBinOp (-) dest src2 src1
+execute (MUL dest src1 src2) = executeBinOp (*) dest src1 src2
 execute HALT = return ()
 
-executeBinArith :: (Int32 -> Int32 -> Int32) -> Destination -> Argument -> Argument -> Run ()
-executeBinArith op dest src1 src2 = do machine <- get
-                                       va <- case (src1) of
-                                                   (ArgC a) -> return a
-                                                   (ArgR a) -> getRegister a
-                                       vb <- case (src2) of
-                                                   (ArgC b) -> return b
-                                                   (ArgR b) -> getRegister b
-                                       setRegister dest (va `op` vb)
-                                       incPC
-                                       run
+executeUnOp  :: (Int32 -> Int32) -> Destination -> Argument a -> Run ()
+executeUnOp  op dest src = do machine <- get
+                              v <- case src of
+                                        (ArgC a) -> return a
+                                        (ArgR a) -> getRegister a
+                              setRegister dest (op v)
+                              incPC
+                              run
+
+executeBinOp :: (Int32 -> Int32 -> Int32) -> Destination -> Argument a -> Argument b -> Run ()
+executeBinOp op dest src1 src2 = do machine <- get
+                                    va <- case (src1) of
+                                                (ArgC a) -> return a
+                                                (ArgR a) -> getRegister a
+                                    vb <- case (src2) of
+                                                (ArgC b) -> return b
+                                                (ArgR b) -> getRegister b
+                                    setRegister dest (va `op` vb)
+                                    incPC
+                                    run
 
 getRegister :: Reg.Register -> Run Int32
 getRegister r = do machine <- get
                    return $ registers machine `Reg.get` r
+
 setRegister :: Reg.Register -> Int32 -> Run ()
 setRegister r v = state $ (\machine -> ((), machine { registers = Reg.set (registers machine) r v }))
 
@@ -64,5 +67,5 @@ testProg :: Memory
 testProg = [MOV Reg.R0 (ArgC 10),
             MOV Reg.R1 (ArgC 20),
             ADD Reg.R2 (ArgR Reg.R0) (ArgR Reg.R1),
-            MUL Reg.R2 (ArgR Reg.R2) Reg.R1,
+            MUL Reg.R2 (ArgR Reg.R2) (ArgR Reg.R1),
             HALT]
--- src/Instructions.hs
+++ src/Instructions.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs, StandaloneDeriving #-}
+
 module Handy.Instructions where
 import Handy.Registers (Register)
 import Data.Int (Int32)
@@ -6,26 +8,26 @@ import Data.List (elem)
 type Destination = Register
 type Constant = Int32
 
-data Argument = ArgC Constant
-              | ArgR Register
-              deriving (Eq)
+data Argument a where
+    ArgC :: Constant -> Argument Constant
+    ArgR :: Register -> Argument Register
 
-instance Show Argument where
+instance Show (Argument a) where
     show (ArgC v) = "#" ++ (show v)
     show (ArgR r) = show r
 
-data Instruction = ADD Destination Argument Argument
-                 | SUB Destination Argument Argument
-                 | RSB Destination Argument Argument
-                 | MUL Destination Argument Register
-                 | CMP Argument Argument
-                 | MOV Destination Argument
-                 | NEG Destination Argument
-                 | B Argument
-                 | BX Argument
-                 | BL Argument
-                 | HALT
-                 deriving (Eq)
+data Instruction where
+    ADD  :: Destination -> Argument a -> Argument a -> Instruction
+    SUB  :: Destination -> Argument a -> Argument a -> Instruction
+    RSB  :: Destination -> Argument a -> Argument a -> Instruction
+    MUL  :: Destination -> Argument a -> Argument Register -> Instruction
+    CMP  :: Argument a  -> Argument a -> Instruction
+    MOV  :: Destination -> Argument a -> Instruction
+    NEG  :: Destination -> Argument a -> Instruction
+    B    :: Argument a  -> Instruction
+    BX   :: Argument a  -> Instruction
+    BL   :: Argument a  -> Instruction
+    HALT :: Instruction
 
 instance Show Instruction where
     show (ADD dest src1 src2) = "ADD " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
