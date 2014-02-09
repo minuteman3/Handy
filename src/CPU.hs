5d62f19d4d9529f8509a01f206ebc71288b5b5d9
Miles McGuire <miles@milesmcguire.com>
2014-02-09 23:16:08 +0000

    Add StatusRegister to CPU and give instructions conditional execution

--- src/CPU.hs
+++ src/CPU.hs
@@ -1,18 +1,26 @@
 {-# LANGUAGE GADTs #-}
 module Handy.CPU where
-import Handy.Instructions
-import qualified Handy.Registers as Reg
+
+import Prelude hiding (EQ,LT,GT)
+
 import Handy.Memory
+import Handy.Instructions
+import Handy.StatusRegister
+import qualified Handy.Registers as Reg
+
 import Control.Monad.State
 import System.IO.Unsafe (unsafePerformIO)
 import Data.Int (Int32)
+import Data.Bits
 
-data Machine = Machine { registers :: Reg.RegisterFile, memory :: Memory } deriving Show
+data Machine = Machine { registers :: Reg.RegisterFile
+                       , memory :: Memory
+                       , cpsr   :: StatusRegister
+                       } deriving Show
 
 type Run a = StateT Machine IO a
 run :: Run ()
 run = do
-    machine <- get
     i <- nextInstruction
     execute i
 
@@ -25,33 +33,94 @@ incPC = do machine <- get
            setRegister Reg.PC $ ((registers machine) `Reg.get` Reg.PC) + 1
 
 execute :: Instruction -> Run ()
-execute (MOV dest src)       = executeUnOp  id  dest src
-execute (ADD dest src1 src2) = executeBinOp (+) dest src1 src2
-execute (SUB dest src1 src2) = executeBinOp (-) dest src1 src2
-execute (RSB dest src1 src2) = executeBinOp (-) dest src2 src1
-execute (MUL dest src1 src2) = executeBinOp (*) dest src1 src2
+execute (MOV cond dest src)       = do machine <- get
+                                       when (checkCondition cond $ cpsr machine) $ do
+                                            let result = executeUnOp id src (registers machine)
+                                            setRegister dest result
+                                       incPC
+                                       run
+
+execute (ADD cond dest src1 src2) = do machine <- get
+                                       when (checkCondition cond $ cpsr machine) $ do
+                                            let result = executeBinOp (+) src1 src2 (registers machine)
+                                            setRegister dest result
+                                       incPC
+                                       run
+
+execute (SUB cond dest src1 src2) = do machine <- get
+                                       when (checkCondition cond $ cpsr machine) $ do
+                                            let result = executeBinOp (-) src1 src2 (registers machine)
+                                            setRegister dest result
+                                       incPC
+                                       run
+
+execute (RSB cond dest src1 src2) = do machine <- get
+                                       when (checkCondition cond $ cpsr machine) $ do
+                                            let result = executeBinOp (-) src2 src1 (registers machine)
+                                            setRegister dest result
+                                       incPC
+                                       run
+
+execute (MUL cond dest src1 src2) = do machine <- get
+                                       when (checkCondition cond $ cpsr machine) $ do
+                                            let result = executeBinOp (*) src1 src2 (registers machine)
+                                            setRegister dest result
+                                       incPC
+                                       run
+
+execute (CMP cond src1 src2)      = do machine <- get
+                                       when (checkCondition cond $ cpsr machine) $ do
+                                           let result = executeBinOp (-) src1 src2 (registers machine)
+                                           let cpsr' = setCPSR result (cpsr machine)
+                                           put $ machine { cpsr = cpsr' }
+                                       incPC
+                                       run
+
 execute HALT = return ()
 
-executeUnOp  :: (Int32 -> Int32) -> Destination -> Argument a -> Run ()
-executeUnOp  op dest src = do machine <- get
-                              v <- case src of
-                                        (ArgC a) -> return a
-                                        (ArgR a) -> getRegister a
-                              setRegister dest (op v)
-                              incPC
-                              run
 
-executeBinOp :: (Int32 -> Int32 -> Int32) -> Destination -> Argument a -> Argument b -> Run ()
-executeBinOp op dest src1 src2 = do machine <- get
-                                    va <- case (src1) of
-                                                (ArgC a) -> return a
-                                                (ArgR a) -> getRegister a
-                                    vb <- case (src2) of
-                                                (ArgC b) -> return b
-                                                (ArgR b) -> getRegister b
-                                    setRegister dest (va `op` vb)
-                                    incPC
-                                    run
+executeUnOp :: (Int32 -> Int32) -> Argument a -> Reg.RegisterFile -> Int32
+executeUnOp op src rf = op v
+                        where v = case src of
+                                   (ArgC a) -> a
+                                   (ArgR a) -> rf `Reg.get` a
+
+executeBinOp :: (Int32 -> Int32 -> Int32) -> Argument a -> Argument b -> Reg.RegisterFile -> Int32
+executeBinOp op src1 src2 rf = va `op` vb
+                               where va = case src1 of
+                                           (ArgC a) -> a
+                                           (ArgR a) -> rf `Reg.get` a
+                                     vb = case (src2) of
+                                           (ArgC b) -> b
+                                           (ArgR b) -> rf `Reg.get` b
+
+checkCondition :: Condition -> StatusRegister -> Bool
+checkCondition AL _ = True
+checkCondition NV _ = False
+checkCondition EQ cpsr = zero cpsr
+checkCondition CS cpsr = carry cpsr
+checkCondition MI cpsr = negative cpsr
+checkCondition VS cpsr = overflow cpsr
+checkCondition HI cpsr = (carry cpsr) && (not $ zero cpsr)
+checkCondition GE cpsr = (negative cpsr) == (overflow cpsr)
+
+checkCondition NE cpsr = not $ checkCondition EQ cpsr
+checkCondition CC cpsr = not $ checkCondition CS cpsr
+checkCondition PL cpsr = not $ checkCondition MI cpsr
+checkCondition VC cpsr = not $ checkCondition VS cpsr
+checkCondition LS cpsr = not $ checkCondition HI cpsr
+checkCondition LT cpsr = not $ checkCondition GE cpsr
+checkCondition GT cpsr = (not $ zero cpsr) && (checkCondition GE cpsr)
+checkCondition LE cpsr = not $ checkCondition GT cpsr
+
+checkCondition HS cpsr = checkCondition CS cpsr
+checkCondition LO cpsr = checkCondition CC cpsr
+
+setCPSR :: Int32 -> StatusRegister -> StatusRegister
+setCPSR result cpsr = cpsr { negative = testBitDefault result 31
+                           , zero     = result == 0
+                           -- TODO: Overflow and Carry flag implementation
+                           }
 
 getRegister :: Reg.Register -> Run Int32
 getRegister r = do machine <- get
@@ -61,11 +130,11 @@ setRegister :: Reg.Register -> Int32 -> Run ()
 setRegister r v = state $ (\machine -> ((), machine { registers = Reg.set (registers machine) r v }))
 
 newMachine :: Memory -> Machine
-newMachine mem = Machine {registers=Reg.blankRegisterFile,memory=mem}
+newMachine mem = Machine {registers=Reg.blankRegisterFile,memory=mem,cpsr=blankStatusRegister}
 
 testProg :: Memory
-testProg = [MOV Reg.R0 (ArgC 10),
-            MOV Reg.R1 (ArgC 20),
-            ADD Reg.R2 (ArgR Reg.R0) (ArgR Reg.R1),
-            MUL Reg.R2 (ArgR Reg.R2) (ArgR Reg.R1),
+testProg = [MOV AL Reg.R0 (ArgC 10),
+            MOV AL Reg.R1 (ArgC 20),
+            ADD AL Reg.R2 (ArgR Reg.R0) (ArgR Reg.R1),
+            MUL AL Reg.R2 (ArgR Reg.R2) (ArgR Reg.R1),
             HALT]
--- src/Instructions.hs
+++ src/Instructions.hs
@@ -16,28 +16,54 @@ instance Show (Argument a) where
     show (ArgC v) = "#" ++ (show v)
     show (ArgR r) = show r
 
+deriving instance Eq a => Eq (Argument a)
+
+data Condition = EQ -- Equal / equals zero  | Zero flag set
+               | NE -- Not equal            | Zero flag clear
+               | CS -- Carry set            | Carry flag set
+               | HS -- Unsigned higher/same | Carry flag set
+               | CC -- Carry clear          | Carry flag clear
+               | LO -- Unsigned lower       | Carry flag clear
+               | MI -- Minus/negative       | Negative flag set
+               | PL -- Plus - postive/zero  | Negative flag clear
+               | VS -- Overflow             | Overflow flag set
+               | VC -- No overflow          | Overflow flag clear
+               | HI -- Unsigned higher      | Carry flag set and Zero flag clear
+               | LS -- Unsigned lower/same  | Carry flag clear or Zero flag set
+               | GE -- Signed greater/equal | Negative flag === Overflow flag
+               | LT -- Signed less than     | Negative flag !== Overflow flag
+               | GT -- Signed greater than  | Zero flag clear and Negative flag === Overflow flag
+               | LE -- Signed less/equal    | Zero flag set or Negative flag !== Overflow flag
+               | AL -- Always               | True
+               | NV -- Never                | False
+               deriving (Show, Eq)
+
 data Instruction where
-    ADD  :: Destination -> Argument a -> Argument a -> Instruction
-    SUB  :: Destination -> Argument a -> Argument a -> Instruction
-    RSB  :: Destination -> Argument a -> Argument a -> Instruction
-    MUL  :: Destination -> Argument a -> Argument Register -> Instruction
-    CMP  :: Argument a  -> Argument a -> Instruction
-    MOV  :: Destination -> Argument a -> Instruction
-    NEG  :: Destination -> Argument a -> Instruction
-    B    :: Argument a  -> Instruction
-    BX   :: Argument a  -> Instruction
-    BL   :: Argument a  -> Instruction
+    ADD  :: Condition -> Destination -> Argument a -> Argument b -> Instruction
+    SUB  :: Condition -> Destination -> Argument a -> Argument b -> Instruction
+    RSB  :: Condition -> Destination -> Argument a -> Argument b -> Instruction
+    MUL  :: Condition -> Destination -> Argument a -> Argument Register -> Instruction
+    CMP  :: Condition -> Argument a  -> Argument b -> Instruction
+    MOV  :: Condition -> Destination -> Argument a -> Instruction
+    NEG  :: Condition -> Destination -> Argument a -> Instruction
+    B    :: Condition -> Argument a  -> Instruction
+    BL   :: Condition -> Argument a  -> Instruction
+    BX   :: Condition -> Argument Register -> Instruction
     HALT :: Instruction
 
 instance Show Instruction where
-    show (ADD dest src1 src2) = "ADD " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
-    show (SUB dest src1 src2) = "SUB " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
-    show (RSB dest src1 src2) = "RSB " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
-    show (MUL dest src1 src2) = "MUL " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
-    show (CMP src1 src2)      = "CMP " ++ show src1 ++ ", " ++ show src2
-    show (MOV dest src1)      = "MOV " ++ show dest ++ ", " ++ show src1
-    show (NEG dest src1)      = "NEG " ++ show dest ++ ", " ++ show src1
-    show (B src1)             = "B "   ++ show src1
-    show (BX src1)            = "BX "  ++ show src1
-    show (BL src1)            = "BL "  ++ show src1
+    show (ADD cond dest src1 src2) = "ADD" ++ show cond ++ " " ++ show dest ++ ", "
+                                           ++ show src1 ++ ", " ++ show src2
+    show (SUB cond dest src1 src2) = "SUB" ++ show cond ++ " " ++ show dest ++ ", "
+                                           ++ show src1 ++ ", " ++ show src2
+    show (RSB cond dest src1 src2) = "RSB" ++ show cond ++ " " ++ show dest ++ ", "
+                                           ++ show src1 ++ ", " ++ show src2
+    show (MUL cond dest src1 src2) = "MUL" ++ show cond ++ " " ++ show dest ++ ", "
+                                           ++ show src1 ++ ", " ++ show src2
+    show (CMP cond src1 src2)      = "CMP" ++ show cond ++ " " ++ show src1 ++ ", " ++ show src2
+    show (MOV cond dest src1)      = "MOV" ++ show cond ++ " " ++ show dest ++ ", " ++ show src1
+    show (NEG cond dest src1)      = "NEG" ++ show cond ++ " " ++ show dest ++ ", " ++ show src1
+    show (B cond src1)             = "B "  ++ show cond ++ show src1
+    show (BX cond src1)            = "BX " ++ show cond ++ show src1
+    show (BL cond src1)            = "BL " ++ show cond ++ show src1
     show (HALT)               = ""
