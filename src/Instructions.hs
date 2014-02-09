b85da3ba83f37c6c7f8510579070de2b94ac1bf7
Miles McGuire <miles@milesmcguire.com>
2014-02-09 17:29:05 +0000

    Pretty printing of instructions

--- src/Instructions.hs
+++ src/Instructions.hs
@@ -8,7 +8,11 @@ type Constant = Int32
 
 data Argument = ArgC Constant
               | ArgR Register
-              deriving (Eq, Show)
+              deriving (Eq)
+
+instance Show Argument where
+    show (ArgC v) = "#" ++ (show v)
+    show (ArgR r) = show r
 
 data Instruction = ADD Destination Argument Argument
                  | SUB Destination Argument Argument
@@ -21,4 +25,17 @@ data Instruction = ADD Destination Argument Argument
                  | BX Argument
                  | BL Argument
                  | HALT
-                 deriving (Eq, Show)
+                 deriving (Eq)
+
+instance Show Instruction where
+    show (ADD dest src1 src2) = "ADD " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
+    show (SUB dest src1 src2) = "SUB " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
+    show (RSB dest src1 src2) = "RSB " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
+    show (MUL dest src1 src2) = "MUL " ++ show dest ++ ", " ++ show src1 ++ ", " ++ show src2
+    show (CMP src1 src2)      = "CMP " ++ show src1 ++ ", " ++ show src2
+    show (MOV dest src1)      = "MOV " ++ show dest ++ ", " ++ show src1
+    show (NEG dest src1)      = "NEG " ++ show dest ++ ", " ++ show src1
+    show (B src1)             = "B "   ++ show src1
+    show (BX src1)            = "BX "  ++ show src1
+    show (BL src1)            = "BL "  ++ show src1
+    show (HALT)               = ""
