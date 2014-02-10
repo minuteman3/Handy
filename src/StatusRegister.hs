93756a1b2ef6b8fd1a69c3e7f9d27936e534eca4
Miles McGuire <miles@milesmcguire.com>
2014-02-10 12:02:28 +0000

    Stop mistakenly using wrong implementation of bit/testBit

--- src/StatusRegister.hs
+++ src/StatusRegister.hs
@@ -17,14 +17,14 @@ blankStatusRegister = StatusRegister { negative = False
 
 toConstant :: StatusRegister -> Int32
 toConstant cpsr = n .|. z .|. c .|. v
-                where n = if negative cpsr then bitDefault 31 else 0
-                      z = if zero cpsr then bitDefault 30 else 0
-                      c = if carry cpsr then bitDefault 29 else 0
-                      v = if overflow cpsr then bitDefault 28 else 0
+                where n = if negative cpsr then bit 31 else 0
+                      z = if zero cpsr then bit 30 else 0
+                      c = if carry cpsr then bit 29 else 0
+                      v = if overflow cpsr then bit 28 else 0
 
 fromConstant :: Int32 -> StatusRegister
-fromConstant val = StatusRegister { negative = testBitDefault val 31
-                                  , zero     = testBitDefault val 30
-                                  , carry    = testBitDefault val 29
-                                  , overflow = testBitDefault val 28
+fromConstant val = StatusRegister { negative = testBit val 31
+                                  , zero     = testBit val 30
+                                  , carry    = testBit val 29
+                                  , overflow = testBit val 28
                                   }
